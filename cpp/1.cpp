71de63cba8d8167e7dfc8edc1713d6b00473f0d6
aaghash-31 <aaghash31@gmail.com>
2026-06-22 11:50:59 +0530

    basic cpp day_1

---
+++ cpp/1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int age=18;
+    // int stud=18;
+    // if(age>=18 && stud>10){
+    //     cout<<"Test can be conducted";
+    // }
+    // else{
+    //     cout<<"Test can't be conducted";
+    // }
+    // char a='A';
+    // switch(a){
+    //     case 'A':
+    //     cout<<1<<endl;
+    //     case 'B':
+    //     cout<<2<<endl;
+    //     case 'C':
+    //     cout<<3<<endl;
+    //     default:
+    //     cout<<0<<endl;
+    // }
+    // int a=5, b=10;
+    // int c=(a>b)?a:b;
+    // cout<<c;
+    int i=0;
+    // for(int i=0;i<10;i++){
+    //     cout<<0<<endl;
+    // }
+    do{
+        cout<<i<<" ";
+        i++;
+    }
+    while(i<5);
+}
